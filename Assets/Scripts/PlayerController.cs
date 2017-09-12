64a0e88302dfd4ee83a53f020caeb7e1c26b18bf
Nate Bhurinat Wangsutthitham <nate.bwangsut@gmail.com>
2017-09-12 16:56:26 +1000

    Add global moving and rotating

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,10 +13,13 @@ public class PlayerController : MonoBehaviour
 	private float gravity = 9.81f;
 
 	private CharacterController _controller;
+	private Rigidbody _rigidbody;
 	
 	// Use this for initialization
 	void Start () {
 		_controller = GetComponent<CharacterController>();
+		_rigidbody = GetComponent<Rigidbody>();
+		
 		
 		movementSpeed = 5;
 		rotationSpeed = movementSpeed * 10;
@@ -28,8 +31,13 @@ public class PlayerController : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 moveDirection = Vector3.zero;
+		Move();
+		Rotate();
+	}
 
+	void Move()
+	{
+		Vector3 moveDirection = Vector3.zero;
 		moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 		
 		if (_controller.isGrounded == false)
@@ -38,12 +46,17 @@ public class PlayerController : MonoBehaviour
 			moveDirection += Physics.gravity;
 		}
 		
-//		Vector3 moveRotation = new Vector3(0, Input.GetAxis("MouseX"), 0);
-//		moveRotation *= rotationSpeed;
-//		transform.Rotate(moveRotation * Time.deltaTime);
-		
-		moveDirection = transform.TransformDirection(moveDirection);
+		// Moving globally
+		//moveDirection = transform.TransformDirection(moveDirection); 
 		moveDirection *= movementSpeed;
 		_controller.Move(moveDirection * Time.deltaTime);
 	}
+
+	void Rotate()
+	{
+		Vector3 moveRotation = new Vector3(0, Input.GetAxis("Mouse X"), 0);
+		moveRotation *= rotationSpeed;
+		transform.Rotate(moveRotation);
+	}
+
 }
