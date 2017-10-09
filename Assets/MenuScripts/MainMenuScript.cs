286c707735a47fe19fda325a84ac68ba70a81d50
Mei Chin <slchin_56@hotmail.com>
2017-10-09 18:51:36 +1100

    Very basic menu up

---
+++ Assets/MenuScripts/MainMenuScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+
+
+	public bool isStart;
+
+	public bool isQuit;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnMouseUp()
+	{
+		if (isStart)
+		{
+			Application.LoadLevel("gameplay");
+		}
+
+		if (isQuit)
+		{
+			Application.Quit();
+			
+			/* Line below to test if this portion of code works
+			   because application would not "quit" in Unity */
+			GetComponent<Renderer>().material.color = Color.cyan;
+
+		}
+	}
+}
---
+++ Assets/MenuScripts/MouseHover.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseHover : MonoBehaviour
+{
+	private Renderer rend;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+		rend = GetComponent<Renderer>();
+		rend.material.color = Color.black;
+
+	}
+
+
+	private void OnMouseEnter()
+	{
+		rend.material.color = Color.yellow;
+	}
+
+	private void OnMouseExit()
+	{
+		rend.material.color = Color.black;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
