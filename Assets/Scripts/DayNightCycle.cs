7b264253e1dba60522c48586b1bf43aadcb6d9fa
Nate Bhurinat Wangsutthitham <nate.bwangsut@gmail.com>
2017-10-10 15:07:32 +1100

    Refactoring
    - Day/Night Cycle

---
+++ Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour {
+	
+	public float speed = (2*Mathf.PI) * 5;
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// Assume x-axis is the of West-East
+		transform.RotateAround(Vector3.zero, Vector3.right, speed * Time.deltaTime);
+		transform.LookAt(Vector3.zero);
+	}
+}
---
+++ Assets/Scripts/Shooting/GunController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+	public bool isFiring;
+	public BulletController bullet;
+
+	public float bulletSpeed;
+	public float cooldown;
+
+	private float shotCounter;
+
+	public Transform firePoint;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isFiring)
+		{
+			shotCounter -= Time.deltaTime;
+			if (shotCounter <= 0)
+			{
+				shotCounter = cooldown;
+				BulletController newBullet = Instantiate(bullet, firePoint.position, firePoint.rotation);
+				newBullet.speed = bullet.speed;
+			}
+		}
+		else
+		{
+			shotCounter = 0;
+		}
+	}
+}
