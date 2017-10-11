a2bd06a735f5724b83ab646eb468917c69a43e3a
Nate Bhurinat Wangsutthitham <nate.bwangsut@gmail.com>
2017-10-11 17:05:53 +1100

    Added Particle Effect to Bullet

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int health;
+	[HideInInspector]
+	private int currentHealth;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		currentHealth = health;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentHealth <= 0)
+		{
+			// Player's Health = 0, gameover
+		}
+	}
+
+	public void damangeEnemy(int damage)
+	{
+		currentHealth -= damage;
+	}
+}
